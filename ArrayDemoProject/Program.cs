9faae028753fd1b16f538df5e607f6a6ba18b8b6
Victoria Castro <victoria.akicastro92@gmail.com>
2024-09-16 21:48:44 -0600

    Proyecto final de curso

---
+++ ArrayDemoProject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string[] coffeTypes;
+float[] coffePrices;
+
+coffeTypes = new string[]{ "Expresso", "Largo", "Filtrado", "Latte" };
+coffePrices = new float[] { 1.2f, 1.5f, 5.0f, 5.5f };
+
+for (int i = 0; i < coffeTypes.Length; i++)
+    Console.WriteLine($"{coffeTypes[i]} coffee = $ {coffePrices[i]} usd");
+
